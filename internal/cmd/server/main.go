cb315b0c68f26b1cd5491a8a27c959ed8a498574
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-09-05 22:16:04 +0330

    Remove unnecessary code for user login functionality in the server main file.

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -1,14 +1,10 @@
 package server
 
 import (
-	"fmt"
 	"github.com/j3yzz/sheriff/internal/delivery/httpserver/server"
 	"github.com/j3yzz/sheriff/internal/infrastructure/config"
 	"github.com/j3yzz/sheriff/internal/infrastructure/db"
 	"github.com/j3yzz/sheriff/internal/repository"
-	"github.com/j3yzz/sheriff/internal/service/authservice"
-	"github.com/j3yzz/sheriff/internal/service/user_service"
-	"github.com/j3yzz/sheriff/internal/service/user_service/userentity"
 	"github.com/spf13/cobra"
 )
 
@@ -21,17 +17,6 @@ func Register(root *cobra.Command) {
 				cfg := config.Provide()
 				database := db.New(cfg.Database)
 				repos := repository.ProvideRepos(database)
-
-				s := user_service.UserService{
-					UserStore: repos.UserRepository,
-					AuthSvc:   authservice.New(cfg.AuthService),
-				}
-				login, err := s.Login(userentity.LoginEntity{
-					Phone:    "09123456789",
-					Password: "123123123123",
-				})
-				fmt.Println("login", login)
-				fmt.Println("err", err)
 
 				server.Provide(repos, cfg)
 			},
