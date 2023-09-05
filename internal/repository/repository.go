12fe2ae3eec7e13fb8f951eedb6be444511b7246
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-09-05 17:55:56 +0330

    Refactor UserRepository instantiation in ProvideRepos function.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,21 +2,17 @@ package repository
 
 import (
 	"github.com/j3yzz/sheriff/internal/infrastructure/db"
-	"github.com/j3yzz/sheriff/internal/service/otptoken_service/otptokenrepo"
 	"github.com/j3yzz/sheriff/internal/service/user_service/userrepo"
 )
 
 type Repositories struct {
-	UserRepository     *userrepo.UserRepository
-	OtpTokenRepository *otptokenrepo.OtpTokenRepository
+	UserRepository *userrepo.UserRepository
 }
 
 func ProvideRepos(db *db.GormDatabase) *Repositories {
 	userRepository := userrepo.New(db)
-	otpTokenRepository := otptokenrepo.New(db)
 
 	return &Repositories{
-		UserRepository:     userRepository,
-		OtpTokenRepository: otpTokenRepository,
+		UserRepository: userRepository,
 	}
 }
