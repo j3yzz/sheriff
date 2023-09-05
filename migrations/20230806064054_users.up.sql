ddf07d001c6e0297733234c2146a714007eaaa1f
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-09-05 17:55:14 +0330

    Add phone_verified_at and password columns to the users table.

--- migrations/20230806064054_users.up.sql
+++ migrations/20230806064054_users.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    phone VARCHAR(12) NOT NULL UNIQUE,
     name VARCHAR(255),
+    phone VARCHAR(12) NOT NULL UNIQUE,
+    phone_verified_at TIMESTAMP,
+    password VARCHAR(255) NOT NULL,
     gender ENUM('male', 'female'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
