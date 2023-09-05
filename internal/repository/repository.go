e3a28440f133465c984a447304c361d6636985a2
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-09-05 23:06:37 +0330

    Add AccessTokenRepository to Repositories in ProvideRepos function.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,17 +2,21 @@ package repository
 
 import (
 	"github.com/j3yzz/sheriff/internal/infrastructure/db"
+	"github.com/j3yzz/sheriff/internal/service/authservice/accesstokenrepo"
 	"github.com/j3yzz/sheriff/internal/service/user_service/userrepo"
 )
 
 type Repositories struct {
-	UserRepository *userrepo.UserRepository
+	UserRepository        *userrepo.UserRepository
+	AccessTokenRepository *accesstokenrepo.AccessTokenRepository
 }
 
 func ProvideRepos(db *db.GormDatabase) *Repositories {
 	userRepository := userrepo.New(db)
+	accessTokenRepository := accesstokenrepo.New(db)
 
 	return &Repositories{
-		UserRepository: userRepository,
+		UserRepository:        userRepository,
+		AccessTokenRepository: accessTokenRepository,
 	}
 }
