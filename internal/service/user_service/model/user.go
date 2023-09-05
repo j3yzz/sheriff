ff5591d80bcc0b791bbe77889bc991ceeb90db3a
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-09-05 18:10:00 +0330

    Refactor User model: added fields PhoneVerifiedAt and Password.

--- internal/service/user_service/model/user.go
+++ internal/service/user_service/model/user.go
@@ -3,10 +3,12 @@ package model
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Phone     string    `json:"phone"`
-	Name      string    `json:"name"`
-	Gender    string    `json:"gender"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID              uint      `gorm:"primaryKey" json:"id"`
+	Name            string    `json:"name"`
+	Phone           string    `json:"phone"`
+	PhoneVerifiedAt time.Time `json:"phone_verified_at"`
+	Password        string    `json:"password"`
+	Gender          string    `json:"gender"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
 }
